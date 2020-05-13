fbce29b73126e9f15d5677e269ac37ca3c983c9a
purnama11 <soni.mkom>
2020-05-13 12:39:27 +0700

    Package Android Done

--- pages/tracking/tracking.py
+++
@@ -1,9 +0,0 @@
-from pages.base import BaseScreen
-
-class PageLacak(BaseScreen):
-    # def __init__(self, **kwargs):
-    #     super().__init__(**kwargs)
-        # self.fetch_data_from_database()
-        # print(self.ids['grid'])
-    pass
-
